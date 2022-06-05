b0d1d391ccc5f5350fbe65152b510366b9364086
zayatanweer <tanweerzaya.1447@gmail.com>
2022-06-05 20:24:46 +0530

    somting i changes in my file

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,12 +3,12 @@ const logger = require('../logger/logger')
 const helper = require('../util/helper')
 const formatter = require('../validator/formatter')
 
-const underscore = require('underscore')
+// const underscore = require('underscore')
 const lodash = require('lodash')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api! Hello Tanweer')
+    res.send('I am Testing My First ever Api')
     logger.zaya()  
     helper.date()
     helper.month()
@@ -17,8 +17,8 @@ router.get('/test-me', function (req, res) {
     formatter.lowerCase()
     formatter.upperCase() 
 })
-router.get('/test-me1', function (req, res) {
-    res.send('My first ever api of the week!') 
+router.get('/hello', function (req, res) {
+    res.send('I Am Testing Hello Api!') 
     
     let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     let subArrays = lodash.chunk(months, 3)
@@ -28,22 +28,19 @@ router.get('/test-me1', function (req, res) {
     let lastNumbers = lodash.tail(oddNumbers)
     console.log('Last 9 odd numbers: ',lastNumbers)
 
-    let arr1 = [1, 2, 3]
-    let arr2 = [2, 3, 4, 4]
-    let arr3 = [4, 5]
-    let arr4 = [4, 6, 4]
-    let arr5 = [5, 8]
-    console.log('Merged array with unique values: ',lodash.union(arr1, arr2, arr3, arr4, arr5))
+    let arr1 = [1, 5, 7]
+    let arr2 = [3, 8, 6]
+    let arr3 = [7, 2, 1]
+    let arr4 = [5, 4, 8]
+    console.log('Merged array with unique values: ',lodash.union(arr1, arr2, arr3, arr4,))
 
-    let movie1 = ['horror', 'The Shining']
-    let movie2 = ['drama','Titanic']
-    let movie3 = ['thriller','Shutter Island']
-    let movie4 = ['fantasy','Pans Labyrinth']
+    let movie1 = ['horror', 'The House Next Door']
+    let movie2 = ['drama','3 Idiots']
+    let movie3 = ['thriller','Saw']
+    let movie4 = ['fantasy','Zumanji']
     let movieObject = lodash.fromPairs([movie1, movie2, movie3, movie4])
     console.log('Movies object: ', movieObject)
             
 })
     
     module.exports = router;
-
-    // let names = [ 'Hariom', 'Arpit', 'Akash', 'Sabiha']
