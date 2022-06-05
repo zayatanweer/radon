62f2744629cffd8136097a1c0abf9bb2cd7d4f32
zayatanweer <tanweerzaya.1447@gmail.com>
2022-06-05 10:41:51 +0530

    modified file once again

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,8 @@ const logger = require('../logger/logger')
 const helper = require('../util/helper')
 const formatter = require('../validator/formatter')
 
-
+const underscore = require('underscore')
+const lodash = require('lodash')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
@@ -15,25 +16,34 @@ router.get('/test-me', function (req, res) {
     formatter.string()
     formatter.lowerCase()
     formatter.upperCase() 
-
-
-});
-
+})
 router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
-});
+    res.send('My first ever api of the week!') 
+    
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    let subArrays = lodash.chunk(months, 3)
+    console.log('Chunks of months: ', subArrays)
+    
+    let oddNumbers = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+    let lastNumbers = lodash.tail(oddNumbers)
+    console.log('Last 9 odd numbers: ',lastNumbers)
 
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
+    let arr1 = [1, 2, 3]
+    let arr2 = [2, 3, 4, 4]
+    let arr3 = [4, 5]
+    let arr4 = [4, 6, 4]
+    let arr5 = [5, 8]
+    console.log('Merged array with unique values: ',lodash.union(arr1, arr2, arr3, arr4, arr5))
 
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
+    let movie1 = ['horror', 'The Shining']
+    let movie2 = ['drama','Titanic']
+    let movie3 = ['thriller','Shutter Island']
+    let movie4 = ['fantasy','Pans Labyrinth']
+    let movieObject = lodash.fromPairs([movie1, movie2, movie3, movie4])
+    console.log('Movies object: ', movieObject)
+            
+})
+    
+    module.exports = router;
 
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
-
-module.exports = router;
-// adding this comment for no reason
+    // let names = [ 'Hariom', 'Arpit', 'Akash', 'Sabiha']
